2c61a8ea85f74c9daad9988d5108eb31e569e5c7
apoorvsingh2314 <apoorvsingh2314@gmail.com>
2022-08-22 16:02:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./App.css";
+// import About from './Components/About';
+import Navbar from "./Components/Navbar";
+import Textform from "./Components/Textform";
+import About from "./Components/About";
+import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [mode, setmode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+    } else {
+      setmode("dark");
+      document.body.style.backgroundColor = "#0b0b45";
+    }
+  };
+  document.body.style.fontFamily = "Georgia, serif";
+
+  return (
+    <>
+      {/* <Router> */}
+        <Navbar title="PlayWithText" mode={mode} toggleMode={toggleMode} />
+        <div className="container">
+          {/* { <Routes> 
+            { <Route
+              path="/"
+              element={
+                />
+               } } */}
+          <Textform heading="Enter Your Text " mode={mode} />
+            {/* <Route path="/about" element={<About />} /> */}
+          {/* </Routes> */}
+        </div>
+      {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
