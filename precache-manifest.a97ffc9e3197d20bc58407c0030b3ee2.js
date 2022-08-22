835254e5e67e22fe151f879af9507629855f3718
apoorvsingh2314 <apoorvsingh2314@gmail.com>
2022-08-22 19:50:07 +0530

    Updates

---
+++ precache-manifest.a97ffc9e3197d20bc58407c0030b3ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc7eaef4893182a8030d",
+    "url": "/PlayWithText/static/css/main.9670f05d.chunk.css"
+  },
+  {
+    "revision": "fc7eaef4893182a8030d",
+    "url": "/PlayWithText/static/js/main.fc7eaef4.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/PlayWithText/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "47a61aeb0ce2eb63fb5a",
+    "url": "/PlayWithText/static/js/2.47a61aeb.chunk.js"
+  },
+  {
+    "revision": "906fb8cc7ac5738da31c",
+    "url": "/PlayWithText/static/js/runtime~main.906fb8cc.js"
+  },
+  {
+    "revision": "3a9ef4844d8a009750dbab6458068742",
+    "url": "/PlayWithText/index.html"
+  }
+];
