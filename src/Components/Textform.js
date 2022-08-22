99ea5fb3c92dda1357a36c882857d6e1ddffe594
apoorvsingh2314 <apoorvsingh2314@gmail.com>
2022-08-23 02:15:50 +0530

    update

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -42,17 +42,17 @@ export default function Textform(props) {
        <div className="mb-3">
        <textarea className="form-control" id="mybox" rows="8" onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#0b0b45':'white',color:props.mode==='dark'?'white':'black'}} value={text}></textarea>
        </div>
-       <button className="btn btn-primary mx-1" onClick={handleUpClick}>ToUpperCase</button>
-       <button className="btn btn-primary mx-1" onClick={handleLoClick}>ToLowerCase</button>
-       <button className="btn btn-primary mx-1" onClick={handleClClick}>Clear Text</button>
-       <button className="btn btn-primary mx-1" onClick={handleCoClick}>Copy Text</button>
-       <button className="btn btn-primary mx-1" onClick={handleExClick}>Remove Extra Space</button>
+       <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>ToUpperCase</button>
+       <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>ToLowerCase</button>
+       <button className="btn btn-primary mx-1 my-1" onClick={handleClClick}>Clear Text</button>
+       <button className="btn btn-primary mx-1 my-1" onClick={handleCoClick}>Copy Text</button>
+       <button className="btn btn-primary mx-1 my-1" onClick={handleExClick}>Remove Extra Space</button>
 
 </div>
 <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
   <h2><b>Your Text Summary</b></h2>
-  <p>{text.split(" ").length} Words and {text.length} Characters</p>
-  <p>{0.008*text.split(" ").length} Minutes Read</p>
+  <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} Words and {text.length} Characters</p>
+  <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes Read</p>
   <h3>Summary</h3>
   <p>{text.length>0?text:"Enter Your Text "}</p>
 </div>
